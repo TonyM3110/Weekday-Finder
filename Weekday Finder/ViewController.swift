aff2edc89f329ef883ebf7ac66482939130b8c1d
Anton Cherevko <anton.ch.tr@gmail.com>
2022-03-12 08:14:29 +0700

    Initial Commit

---
+++ Weekday Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weekday Finder
+//
+//  Created by Tony on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
