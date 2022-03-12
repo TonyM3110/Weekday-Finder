21b4b73a22b1d08811416832df5d048c02a5608d
Tony Cherevko <anton.ch.tr@gmail.com>
2022-03-12 09:09:32 +0700

    Simple Week Day App
    My first App. U can find the week day by entering day, month and year. (Language: Russian)

--- Weekday Finder/ViewController.swift
+++ Weekday Finder/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var DateTextField: UITextField!
+    @IBOutlet weak var MonthTextField: UITextField!
+    @IBOutlet weak var YearTextField: UITextField!
+    @IBOutlet weak var resultLable: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = DateTextField.text, let month = MonthTextField.text, let year = YearTextField.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponets = DateComponents()
+        dateComponets.day = Int(day)
+        dateComponets.month = Int(month)
+        dateComponets.year = Int(year)
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        guard let date = calendar.date(from: dateComponets) else { return }
+        let weekDay = dateFormatter.string(from: date)
+        let capitalisedWeekDay = weekDay.capitalized
+        resultLable.text = capitalisedWeekDay
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
